f75174893a814417544acc32c3b45b51662f807d
Megan Anthony <mantho965@cable.comcast.com>
2020-04-29 21:44:17 -0400

    form made

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProjectOverview from '../src/pages/Project-Overview';
+import Form  from '../src/components/taskFormComponent'
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path='/' component={ProjectOverview} />
+        <Route exact path='/form' component={Form}/>
+      </Switch>
     </div>
-  );
-}
+  </Router>
+)
 
 export default App;
+
