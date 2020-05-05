108cfc6aa6880767017ea481c9d458fb49d83f84
nbennett <nbennett@linode.com>
2020-05-05 08:16:45 -0400

    Revert "started testing branch for db connection; not sure how this will work!"
    This reverts commit b8af5f682f5184661878980b8d09b61663b8cea5.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [merchants, setMerchants] = useState(false);
-
-  useEffect(() => {
-    getMerchant();
-  }, []);
-
-  function getMerchant() {
-    fetch('http://localhost:3001')
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        setMerchants(data);
-      });
-  }
-
-  function createMerchant() {
-    let name = prompt('Enter merchant name');
-    let email = prompt('Enter merchant email');
-
-    fetch('http://localhost:3001/merchants', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({name, email}),
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        alert(data);
-        getMerchant();
-      });
-  }
-
-  function deleteMerchant() {
-    let id = prompt('Enter merchant id');
-
-    fetch(`http://localhost:3001/merchants/${id}`, {
-      method: 'DELETE',
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        alert(data);
-        getMerchant();
-      });
-  }
-
   return (
-    <div>
-      {merchants ? merchants : 'There is no merchant data available'}
-      <br />
-      <button onClick={createMerchant}>Add</button>
-      <br />
-      <button onClick={deleteMerchant}>Delete</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
